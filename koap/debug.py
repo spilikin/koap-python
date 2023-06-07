aa0046735e2440d54def3354be1263b3607a31ad
spilikin <108782+spilikin@users.noreply.github.com>
2023-06-07 11:10:25 +0200

    Add elapsed time to requests

--- koap/debug.py
+++ koap/debug.py
@@ -2,10 +2,13 @@ from rich.console import Console
 from rich.terminal_theme import NIGHT_OWLISH as THEME 
 from zeep import Plugin
 from lxml import etree
+import datetime
 
 
 class RichSoapDebugPlugin(Plugin):
-  
+    # timestamp of the last request
+    last_request_timestamp = None
+
     def __init__(self, debug_console: Console = None):
         if debug_console is None:
             self.debug_console = Console(record=True)
@@ -13,12 +16,14 @@ class RichSoapDebugPlugin(Plugin):
             self.debug_console = debug_console
 
     def ingress(self, envelope, http_headers, operation):
-        self.debug_console.print("Response:")
+        elapsed_time = datetime.datetime.now() - self.last_request_timestamp
+        self.debug_console.print(f"Response ({elapsed_time}):")
         self.debug_console.print(etree.tostring(envelope, pretty_print=True).decode())
 
         return envelope, http_headers
 
     def egress(self, envelope, http_headers, operation, binding_options):
+        self.last_request_timestamp = datetime.datetime.now()
         self.debug_console.print("Request:")
         self.debug_console.print(etree.tostring(envelope, pretty_print=True).decode())
 
